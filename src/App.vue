98626e543aa1adb05a3594e45b3e58be679a60f1
Baddeveloper <stijn.simons2@gmail.com>
2018-10-02 20:49:07 +0200

    Added Dutch translation

--- src/App.vue
+++ src/App.vue
@@ -12,6 +12,7 @@
               <div @click="chooseBookTranslation('en')">English</div>
               <div @click="chooseBookTranslation('de')">Deutsche</div>
 	      <div @click="chooseBookTranslation('fr')">Français</div>
+	      <div @click="chooseBookTranslation('nl')">Nederlands</div>
             </div>
             <div class="translations-info">
               Translations have different numbers of pages
